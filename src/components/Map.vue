529b103651e4ba514e1f0e1f4d2d8b4f971064bb
vprint <valentin.printemps@gmail.com>
2023-04-15 14:34:34 +0200

    Initialisation du projet
    Initialisation de vueJS et d'ol. Ajout d'une barre latérale sur la carte.

---
+++ src/components/Map.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="map"></div>
+</template>
+
+<script>
+import { SidePanel } from 'ol-side-panel';
+import { Map, View } from 'ol';
+import { Tile as TileLayer } from 'ol/layer';
+import { OSM } from 'ol/source';
+
+export default {
+    name: 'Map',
+    mounted() {
+        this.map = new Map({
+            target: 'map',
+            layers: [
+                new TileLayer({
+                    source: new OSM()
+                })
+            ],
+            view: new View({
+                center: [0, 0],
+                zoom: 2
+            })
+        })
+        let sidePanel = new SidePanel();
+        this.map.addControl(sidePanel);
+
+        let layersPane = sidePanel.definePane({
+            paneId: 'layers',
+            name: "Layers",
+            icon: '<i class="fg-regular-shape-pt"></i>'
+        });
+    }
+}
+</script>
